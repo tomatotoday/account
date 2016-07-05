cb09455974f270ddf953670dcb5f768a2048717d
soasme <soasme@gmail.com>
2016-07-05 17:30:09 +0800

    Define account service.

--- tomato/account/jsonrpc.py
+++ tomato/account/jsonrpc.py
@@ -2,12 +2,20 @@
 
 from time import mktime
 from datetime import datetime
+from datetime import timedelta
+
+from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 
 from tomato.account.core import db
 from tomato.account.core import jsonrpc
 from tomato.account.models import Client
 from tomato.account.models import Grant
 from tomato.account.models import Token
+from tomato.account.models import User
+from tomato.account.models import UserEmail
+from tomato.account.models import UserAuth
 
 @jsonrpc.method('OAuth2.get_token_by_access_token')
 def get_token_by_access_token(access_token):
@@ -33,7 +41,7 @@ def save_token(client_id, user_id, expires_in, access_token,
         access_token=access_token,
         refresh_token=refresh_token,
         token_type=token_type,
-        scopes=scopes,
+        _scopes=scopes,
         expires=expires,
         client_id=client_id,
         user_id=user_id,
@@ -45,16 +53,7 @@ def save_token(client_id, user_id, expires_in, access_token,
 @jsonrpc.method('OAuth2.get_client')
 def get_client(client_id):
     client = Client.query.filter_by(client_id=client_id).first()
-    return client and dict(
-        name=client.name,
-        description=client.description,
-        user_id=client.user_id,
-        client_id=client.client_id,
-        client_secret=client.client_secret,
-        client_type=client.client_type,
-        redirect_uris=client.redirect_uris,
-        default_scopes=client.default_scopes,
-    )
+    return client and client.to_dict()
 
 @jsonrpc.method('OAuth2.get_grant')
 def get_grant(client_id, code):
@@ -75,3 +74,40 @@ def save_grant(client_id, code, redirect_uri, scopes, user_id):
     db.session.add(grant)
     db.session.commit()
     return grant.to_dict()
+
+@jsonrpc.method('Account.validate_user')
+def validate_user(username, password):
+    email = UserEmail.query.filter_by(email=username).first()
+    if not email:
+        return
+    user_id = email.user_id
+    auth = UserAuth.query.filter_by(user_id=user_id).first()
+    if not auth:
+        return
+    if not check_password_hash(auth.password, password):
+        return
+    return email.user.to_dict()
+
+@jsonrpc.method('Account.get_user_by_token')
+def get_user_by_token(token, token_type='bearer'):
+    token = Token.query.filter_by(token=token).first()
+    if not token:
+        return
+    return token.user.to_dict()
+
+@jsonrpc.method('Account.register_user_by_email')
+def register_user_by_email(nickname, email, password):
+    try:
+        user = User(nickname=nickname, is_enabled=True)
+        db.session.add(user)
+        user.flush()
+        user_email = UserEmail(user_id=user.id, email=email, is_primary=True)
+        db.session.add(user_email)
+        user_email.flush()
+        salted_password = generate_password_hash(password)
+        user_auth = UserAuth(user_id=user.id, password=salted_password)
+        db.session.add(user_auth)
+        db.session.commit()
+        return user.to_dict()
+    except IntegrityError:
+        db.session.rollback()
